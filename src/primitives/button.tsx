68392b3fc60194d0b80af95c74054c999ad9caf3
Trisha Gonzales <gonzalestrishaanne@gmail.com>
2024-03-04 08:34:59 +0800

    button variant styles

--- src/primitives/button.tsx
+++ src/primitives/button.tsx
@@ -8,45 +8,43 @@ import {
 import { twMerge } from 'tailwind-merge';
 import cs from './button.module.scss';
 
-type ButtonVariant = 'solid' | 'outline' | 'muted' | 'ghost' | 'text';
+type Variant = 'solid' | 'outline' | 'muted' | 'ghost' | 'text';
 
 type BaseProps = {
-  variant?: ButtonVariant;
+  variant?: Variant;
 } & React.HTMLAttributes<HTMLButtonElement>;
 
-type ButtonProps = {
-  isIcon: false;
-} & BaseProps &
+type ButtonProps = BaseProps &
   Omit<MantineButtonProps, 'variant' | 'classNames'>;
 
-type ButtonIconProps = {
-  isIcon: true;
-} & BaseProps &
+type ButtonIconProps = BaseProps &
   Omit<MantineActionIconProps, 'variant' | 'classNames'>;
 
-const VariantMap = {
-  solid: 'filled',
-  outline: 'outline',
-  muted: 'light',
-  ghost: 'subtle',
-  text: 'transparent',
-};
+function Button(props: ButtonProps) {
+  const { variant = 'muted', className, ...rest } = props;
 
-export default function Button({
-  variant = 'muted',
-  className,
-  ...rest
-}: ButtonProps | ButtonIconProps) {
-  const props = {
-    color: 'accent',
-    variant: VariantMap[variant],
-    className: twMerge(cs.root, className),
-    ...rest,
-  };
-
-  return props.isIcon ? (
-    <ActionIcon size='lg' {...props} />
-  ) : (
-    <MantineButton {...props} />
+  return (
+    <MantineButton
+      color='accent'
+      variant={variant}
+      className={twMerge(cs.root, className)}
+      {...rest}
+    />
   );
 }
+
+function Icon(props: ButtonIconProps) {
+  const { variant = 'muted', className, ...rest } = props;
+
+  return (
+    <ActionIcon
+      size='lg'
+      color='accent'
+      variant={variant}
+      className={twMerge(cs.root, className)}
+      {...rest}
+    />
+  );
+}
+
+export default Object.assign(Button, { Icon });
